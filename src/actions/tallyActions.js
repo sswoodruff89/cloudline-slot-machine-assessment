5576650493fb53f304f70441b21cf5a6999dcc89
sean <sswoodruff89@gmail.com>
2020-10-30 13:15:41 -0400

    made tally actions

--- src/actions/tallyActions.js
+++ src/actions/tallyActions.js
@@ -1,4 +1,4 @@
-import { ADD_TO_WINS } from "./types";
+import { ADD_TO_WINS, ADD_TO_TRIES, RESET_TALLY } from "./types";
 
 export const addToWins = () => dispatch => {
   dispatch({ type: ADD_TO_WINS, payload: null });
@@ -8,8 +8,20 @@ export const addToWins = () => dispatch => {
 // Create an action creator called addToTries, which dispatches
 // a type of ADD_TO_TRIES (which should be exported from types.js)
 // The payload can simply be null, as we're counting in the reducer.
-
+export const addToTries = () => dispatch => {
+  dispatch({
+    type: ADD_TO_TRIES,
+    payload: null
+  })
+}
 // TASK
 // Create an action creator called resetTally, which dispatches
 // a type of RESET_TALLY.
 // The payload can simply be null, as we're resetting values in the reducer.
+
+export const resetTally = () => dispatch => {
+  dispatch({
+    type: RESET_TALLY,
+    payload: null
+  })
+}
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
 export const ADD_TO_WINS = "add_to_wins";
+export const ADD_TO_TRIES = "add_to_tries";
+export const RESET_TALLY = "reset_tries";
