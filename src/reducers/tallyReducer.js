707ed5f993b5d2dd30703d97bceffc36b33ccbe8
sean <sswoodruff89@gmail.com>
2020-10-30 13:19:10 -0400

    filled tally reducer

--- src/reducers/tallyReducer.js
+++ src/reducers/tallyReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_WINS } from "../actions/types";
+import { ADD_TO_WINS, ADD_TO_TRIES, RESET_TALLY } from "../actions/types";
 
 const tallyState = { wins: 0, tries: 0 };
 
@@ -9,9 +9,13 @@ export const listTally = (state = tallyState, action) => {
 
     // TASK
     // Increase the number of tries by 1 with type ADD_TO_TRIES
+    case ADD_TO_TRIES:
+      return { ...state, tries: (state.tries += 1)}
 
     // TASK
     // Reset the state values back to 0 with type RESET_TALLY
+    case RESET_TALLY:
+      return {...state, ...tallyState}
 
     default:
       return state;
